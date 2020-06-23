59c0bb892537ee6e1225b1407bee594da9834e75
Niketa Gandhi <niketa282@gmail.com>
2020-06-23 13:28:10 +0530

    Add first file

---
+++ photoresitor_jun15a.ino
@@ -0,0 +1,20 @@
+#define INPUT_PIN A0
+#define LED_BUILT_IN 13
+int threshold=15;
+void setup() {
+  pinMode(INPUT_PIN,OUTPUT);
+  pinMode(LED_BUILT_IN,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ Serial.println(analogRead(INPUT_PIN));
+  delay(100);
+  if(analogRead(INPUT_PIN)< threshold){
+    digitalWrite(LED_BUILT_IN,HIGH);
+    }
+    else{
+      digitalWrite(LED_BUILT_IN,LOW);
+      }
+}
